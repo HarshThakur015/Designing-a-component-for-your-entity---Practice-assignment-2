da891b8ee92d681fd1c41d263f18c535fa4e2e90
Harsh Thakur <tsayamonly@gmail.com>
2025-03-12 10:23:15 +0530

    Update productcard.jsx

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -4,9 +4,9 @@ import Button from "./button";
 
 export default function productcard() {
   const product_image =
-    "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wfGVufDB8fDB8fHww";
+    "https://i.pinimg.com/736x/41/59/91/415991758da277ff47dc2bf33554468d.jpg";
 
-  const product_name = "ASUS TUF Gaming A15, AMD Ryzen 7";
+  const product_name = "HP Victus, Intel Core i5";
 
   const price = "63990";
   return (
